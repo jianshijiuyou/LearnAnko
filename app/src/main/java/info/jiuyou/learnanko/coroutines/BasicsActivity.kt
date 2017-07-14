9c7ff3ebc632d45e61e96f5c96b5a8063d86e5b3
jianshijiuyou <jianshijiuyou@gmail.com>
2017-07-14 10:16:36 +0800

    update

---
+++ app/src/main/java/info/jiuyou/learnanko/coroutines/BasicsActivity.kt
@@ -0,0 +1,14 @@
+package info.jiuyou.learnanko.coroutines
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import info.jiuyou.learnanko.R
+
+class BasicsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basics)
+    }
+}
